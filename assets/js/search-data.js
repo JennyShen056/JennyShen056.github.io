cfb1b8dfda4ceb6dcfaa3f44bfaa7aaf12793d83
JennyShen056 <112578566+JennyShen056@users.noreply.github.com>
2026-05-19 00:40:19 +0000

    Deploying to gh-pages from @ JennyShen056/JennyShen056.github.io@4e51fb3eacc83d907de64c020cab89139339b64e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -449,13 +449,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/jenny_cv.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
